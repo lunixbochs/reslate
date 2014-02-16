58405395ff3045ce152b34653d42d7b502949bd4
Dean Sofer <proloser@hotmail.com>
2014-02-16 14:05:48 -0800

    Simplified bindings to leverage command chaining
    Instead of having 11 hard-to-remember bindings, IJKL and the Arrow keys
    will iterate through all common window arrangements, and then proceed
    to move the window to the next monitor.
    
    This closer resembles the behavior found on Windows 7+ by hitting
    [WIN]+[LEFT] or [RIGHT]

--- slate.example.js
+++ slate.example.js
@@ -28,56 +28,44 @@ slate.configAll({
 });
 
 // bindings
+var left = [
+    $('barResize', 'left', 2),
+    $('barResize', 'left', 1.5),
+    $('toss', '-', 'resize')
+];
+var right = [
+    $('barResize', 'right', 2),
+    $('barResize', 'right', 3),
+    $('toss', '+', 'resize')
+];
+var up = [
+    $('barResize', 'top', 2),
+    $('corner', 'top-left', 2, 2),
+    $('corner', 'top-right', 2, 2),
+    $('toss', '0', 'resize')
+];
+var down = [
+    $('barResize', 'bottom', 2),
+    $('corner', 'bottom-left', 2, 2),
+    $('corner', 'bottom-right', 2, 2),
+    $('toss', '0', 'resize')
+];
+ 
+// bindings
 slate.bindAll({
     hyper: {
-        shift: {
-            // edges
-            h: [$('barResize', 'left',   3),
-                $('center',    'left',   3, 3)],
-            j: [$('barResize', 'bottom', 2),
-                $('center',    'bottom', 3, 3)],
-            k: [$('barResize', 'top',    2),
-                $('center',    'top',    3, 3)],
-            l: [$('barResize', 'right',  3),
-                $('center',    'right',  3, 3)],
-
-            // corners
-            y: [$('corner', 'top-left',     3, 2),
-                $('corner', 'top-left',     3, 3)],
-            i: [$('corner', 'top-right',    3, 2),
-                $('corner', 'top-right',    3, 3)],
-            b: [$('corner', 'bottom-left',  3, 2),
-                $('corner', 'bottom-left',  3, 3)],
-            m: [$('corner', 'bottom-right', 3, 2),
-                $('corner', 'bottom-right', 3, 3)],
-
-            // centers
-            u: [$('center', 'top'),
-                $('center', 'top', 3, 3)],
-            n: [$('center', 'bottom'),
-                $('center', 'bottom', 3, 3)],
-            'return': $('center', 'center', 3, 3)
-        },
-        // bars
-        h: [$('barResize', 'left',  2),
-            $('barResize', 'left',  1.5)],
-        j: $('barResize', 'bottom', 2),
-        k: $('barResize', 'top',    2),
-        l: [$('barResize', 'right', 2),
-            $('barResize', 'right', 1.5)],
-        // corners
-        y: [$('corner', 'top-left'),
-            $('corner', 'top-left', 1.5)],
-        i: [$('corner', 'top-right'),
-            $('corner', 'top-right', 1.5)],
-        b: [$('corner', 'bottom-left'),
-            $('corner', 'bottom-left', 1.5)],
-        m: [$('corner', 'bottom-right'),
-            $('corner', 'bottom-right', 1.5)],
-        // centers
-        u: $('center', 'top'),
-        n: $('center', 'bottom'),
-        'return': $('center', 'center'),
+        left: left,
+        j: left,
+        right: right,
+        l: right,
+        up: up,
+        i: up,
+        down: down,
+        k: down,
+        'return': [
+            $('barResize', 'left', 1),
+            $('center', 'center', 1.5, 1.25)
+        ],
         // throw to monitor
         '`': ['throw 0 resize',
               'throw 1 resize'],
